9b9ccfea2d60d6052e4fb4c1d46aed0f9a5d8356
iVampireSP.com <im@ivampiresp.com>
2022-04-08 10:17:20 +0800

    增加 区分自动暂停还是人工暂停的迁移

---
+++ database/migrations/2022_04_08_021623_add_auto_suspend_to_orders.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            // 区别是自动暂停还是人工暂停
+            $table->boolean('auto_suspend')->after('suspended_at')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
+    }
+};
